4e1502615a2c9741144fee9335b2efb772091414
sing3demons <sing3demons@live.com>
2021-06-12 13:45:43 +0700

    http status

--- src/main/java/com/sing3demons/stock_backend/controller/ProductController.java
+++ src/main/java/com/sing3demons/stock_backend/controller/ProductController.java
@@ -4,9 +4,6 @@ import com.sing3demons.stock_backend.exception.ProductNotFoundException;
 import com.sing3demons.stock_backend.models.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
-import java.nio.file.ProviderNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -18,21 +15,25 @@ public class ProductController {
     private final AtomicLong counter = new AtomicLong();
     private List<Product> productList = new ArrayList<>();
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("")
     public List<Product> getProducts() {
         return productList;
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable(name = "id") long id) {
         return productList.stream().filter(p -> p.getId() == id).findFirst().orElseThrow(() -> new ProductNotFoundException(id));
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/search")
     public String getProduct(@RequestParam(name = "name") String name) {
         return "search " + name;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
     public Product createProducts(@RequestBody Product product) {
         Product data = new Product(counter.incrementAndGet(), product.getName(), product.getImage(), product.getPrice(), product.getStock());
@@ -40,6 +41,7 @@ public class ProductController {
         return data;
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{id}")
     public void update(@RequestBody Product product, @PathVariable("id") long id) {
         Product data;
@@ -53,6 +55,7 @@ public class ProductController {
         });
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") long id) {
         productList.stream().filter(p -> p.getId() == id).findFirst().ifPresentOrElse(r -> {
