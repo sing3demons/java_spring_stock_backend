662a41891b129c6888a24b25a7c7690fd02e7a13
sing3demons <sing3demons@live.com>
2021-06-12 13:57:17 +0700

    Using Lombok models/Product

--- src/main/java/com/sing3demons/stock_backend/models/Product.java
+++ src/main/java/com/sing3demons/stock_backend/models/Product.java
@@ -1,57 +1,13 @@
 package com.sing3demons.stock_backend.models;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
 public class Product {
     private long id;
     private String name;
     private String image;
     private int price;
     private int stock;
-
-    public Product(long id, String name, String image, int price, int stock) {
-        this.id = id;
-        this.name = name;
-        this.image = image;
-        this.price = price;
-        this.stock = stock;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 }
