53f0bca1b99514a2db23dee5113549999cd5bbf9
sing3demons <sing3demons@live.com>
2021-06-12 13:12:22 +0700

    update and delete product no database

--- src/main/java/com/sing3demons/stock_backend/controller/ProductController.java
+++ src/main/java/com/sing3demons/stock_backend/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.sing3demons.stock_backend.controller;
 import com.sing3demons.stock_backend.models.Product;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -20,7 +21,7 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public Product getProduct(@PathVariable(name = "id") int id) {
+    public Product getProduct(@PathVariable(name = "id") long id) {
         return productList.stream().filter(p -> p.getId() == id).findFirst().orElseThrow();
     }
 
@@ -34,6 +35,28 @@ public class ProductController {
         Product data = new Product(counter.incrementAndGet(), product.getName(), product.getImage(), product.getPrice(), product.getStock());
         productList.add(data);
         return data;
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody Product product, @PathVariable("id") long id) {
+        Product data;
+        productList.stream().filter(p -> p.getId() == id).findFirst().ifPresentOrElse(r -> {
+            r.setName(product.getName());
+            r.setImage(product.getImage());
+            r.setPrice(product.getPrice());
+            r.setStock(product.getStock());
+        }, () -> {
+            //TODO
+        });
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") long id) {
+        productList.stream().filter(p -> p.getId() == id).findFirst().ifPresentOrElse(r -> {
+            productList.remove(r);
+        }, () -> {
+            //TODO
+        });
     }
 
 }
