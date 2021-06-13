74342e7a6efac73fdc01b4cf5e28f64765277047
sing3demons <sing3demons@live.com>
2021-06-13 11:13:53 +0700

    java-dotenv

--- src/main/java/com/sing3demons/stock_backend/StockBackendApplication.java
+++ src/main/java/com/sing3demons/stock_backend/StockBackendApplication.java
@@ -1,21 +1,42 @@
 package com.sing3demons.stock_backend;
 
 import com.sing3demons.stock_backend.service.StorageService;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class StockBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockBackendApplication.class, args);
+        Map<String, Object> env = Dotenv.load()
+                .entries().stream()
+                .collect(Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
+
+        new SpringApplicationBuilder(StockBackendApplication.class)
+                .environment(new StandardEnvironment() {
+                    @Override
+                    protected void customizePropertySources(@NotNull MutablePropertySources propertySources) {
+                        super.customizePropertySources(propertySources);
+                        propertySources.addLast(new MapPropertySource("dotenvProperties", env));
+                    }
+                }).run(args);
+//        SpringApplication.run(StockBackendApplication.class, args);
     }
 
     @Bean
-    CommandLineRunner init(StorageService storageservice){
-        return args-> storageservice.init();
+    CommandLineRunner init(StorageService storageservice) {
+        return args -> storageservice.init();
     }
 
 }
--- src/main/java/com/sing3demons/stock_backend/config/WebConfiguration.java
+++ src/main/java/com/sing3demons/stock_backend/config/WebConfiguration.java
@@ -1,14 +1,18 @@
 package com.sing3demons.stock_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
+    @Value("${app.upload.path:images}")
+    private String path;
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/images/");
+        registry.addResourceHandler("/api/" + path + "/*")
+                .addResourceLocations("file:" + path + "/");
     }
 }
