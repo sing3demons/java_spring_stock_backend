b466645e5d2358ceb715dc96b28908f805d30763
sing3demons <sing3demons@live.com>
2021-06-13 09:31:50 +0700

    Profile with properties file

--- src/main/java/com/sing3demons/stock_backend/controller/ProductController.java
+++ src/main/java/com/sing3demons/stock_backend/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.sing3demons.stock_backend.models.Product;
 import com.sing3demons.stock_backend.request.ProductRequest;
 import com.sing3demons.stock_backend.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +19,13 @@ import java.util.List;
 @Slf4j
 public class ProductController {
 
-    private final ProductService productService;
+    @Autowired
+    ProductService productService;
 
-    ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+//    private final ProductService productService;
+//    ProductController(ProductService productService) {
+//        this.productService = productService;
+//    }
 
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
